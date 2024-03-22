508ef426371aab2b1eefe4caf4dd15c8a6727c34
Shoshano <shoshano2048@gmail.com>
2024-03-22 08:58:15 +0100

    Dodaj pliki projektów.

---
+++ Projekt2_Aplikacja_bazodanowa_API/APITest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Projekt2_Aplikacja_bazodanowa_API
+{
+    internal class APITest
+    {
+        public HttpClient Client { get; set; }
+        public async Task GetData()
+        {
+            Client = new HttpClient();
+            string call = "http://radoslaw.idzikowski.staff.iiar.pwr.wroc.pl/instruction/students.json"; //"https://swapi.dev/api/";
+            string response = await Client.GetStringAsync(call);
+            List<Student> students = JsonSerializer.Deserialize<List<Student>>(response);
+            foreach(var student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+            
+        }
+    }
+}
